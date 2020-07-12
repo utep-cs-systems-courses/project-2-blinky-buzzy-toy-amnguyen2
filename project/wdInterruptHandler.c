de7b02edf94614b122039ca5b695a8a96c7b30cc
amnguyen2 <abramnguyen210@gmail.com>
2020-07-12 01:09:28 -0600

    update led and buzzer when interrupt

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -1,10 +1,15 @@
 #include <msp430.h>
 #include "stateMachines.h"
+#include "led.h"
 
 void __interrupt_vec(WDT_VECTOR) WDT() { /* 250 interrupts/sec */
   static char blink_count = 0;
-  if(++blink_count == 125) {
+  if(++blink_count > 125) {
     state_advance();
     blink_count = 0;
   }
+  
+  sm_update_led();
+  sm_update_buzzer();
+  led_update();
 }
